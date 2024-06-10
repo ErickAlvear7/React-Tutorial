9252295abef8b734cf30f33f3205404908fb0284
Erick <erick.alvear7@gmail.com>
2024-06-10 18:40:36 -0500

    desde otra rama

--- src/components/ContadorComponent.jsx
+++ src/components/ContadorComponent.jsx
@@ -2,7 +2,7 @@
 export const ContadorComponent = () => {
   return (
     <>
-      <h1>Contador: 0</h1>
+      <h1 className="mb-2">Contador: 0</h1>
       <div className="container mt-3">
         <button className="btn btn-success me-2" onClick={ () => incrementar()}>+1</button>
         <button className="btn btn-outline-danger me-2" onClick={ () => resetear()}>Reset</button>
